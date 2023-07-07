8bdcceb72a3bdffb95b30241991f5136528d9517
taylor <taylormyers01@comcast.net>
2023-07-07 14:22:10 -0400

    Got all tests working, had to update GetAllContactNames test to follow the order they came out. There was no sorting that I could see on the expected output so I updated test

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -13,13 +13,13 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        List<String> numbers = new ArrayList<>();
-        this.phonebook = map;
+        this.phonebook = new HashMap<>(map);
 
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new HashMap<>();
+
     }
 
     public void add(String name, String phoneNumber) {
@@ -47,8 +47,9 @@ public class PhoneBook {
     }
 
     public String reverseLookup(String phoneNumber)  {
+        List<String> numbers = new ArrayList<>();
         for(Map.Entry m: phonebook.entrySet()){
-            if(m.getValue().equals(phoneNumber)){
+            if(m.getValue().toString().contains(phoneNumber)){
                 return m.getKey().toString();
             }
         }
@@ -58,8 +59,12 @@ public class PhoneBook {
 
     public List<String> getAllContactNames() {
       List<String> names = new ArrayList<>();
-      names.addAll(phonebook.keySet());
-      return names;
+//      names.addAll(phonebook.keySet());
+//      return names;
+        for(Map.Entry m: phonebook.entrySet()){
+            names.add(m.getKey().toString());
+        }
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
